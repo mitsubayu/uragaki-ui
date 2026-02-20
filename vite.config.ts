6a6c2d3e1b96111b142bd6b64a1b18e05ef0d206
YukiIzumoto <yu-izumoto@kddi-agile.com>
2026-02-20 23:42:40 +0900

    Fix casing for "Uragaki-UI" in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/yu-izumoto/uragaki-ui/",
+  base: "/yu-izumoto/Uragaki-UI/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
